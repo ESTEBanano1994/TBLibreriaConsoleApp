c1501f4796a3260ea0af729049c1543e813a060d
Esteban Interiano <jinteriano-2023449@kinal.edu.gt>
2026-06-19 10:02:04 -0600

    001 se agrego el package de dao y se modifico su impl

--- src/org/tb/system/Main.java
+++ src/org/tb/system/Main.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package org.tb.system;
+
+import org.tb.view.MenuPrincipal;
 
 /**
  *
@@ -10,11 +9,10 @@ package org.tb.system;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciar();
     }
     
     
