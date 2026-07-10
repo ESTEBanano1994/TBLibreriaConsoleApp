ab83b67c8f50460d6574f3a44f7a51afd1c8414e
Antony Perez <aperez-2023350@kinal.edu.gt>
2026-07-10 10:59:00 -0600

    feat(editorial) Se agrego el EditorialDAOimpl #12

--- src/org/tb/dao/impl/EditorialDAOimpl.java
+++ src/org/tb/dao/impl/EditorialDAOimpl.java
@@ -1,62 +1,47 @@
 package org.tb.dao.impl;
- 
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
- 
 import org.tb.dao.EditorialDao;
 import org.tb.model.Editorial;
 import org.tb.util.Conexion;
- 
 public class EditorialDAOimpl implements EditorialDao {
     @Override
 public List<Editorial> listarTodos() {
- 
     List<Editorial> editoriales = new ArrayList<>();
- 
     String consulta = "{call sp_listareditoriales()}";
- 
     try (
         Connection conexion = Conexion.getInstancia().conectar();
         CallableStatement cs = conexion.prepareCall(consulta);
         ResultSet rs = cs.executeQuery()
     ) {
- 
         while (rs.next()) {
- 
             Editorial edi = new Editorial();
- 
             edi.setNit(rs.getString("nit"));
             edi.setNombre_editorial(rs.getString("nombre_editorial"));
             edi.setTelefono_editorial(rs.getString("telefono_editorial"));
             edi.setDireccion_editoria(rs.getString("direccion_editoria"));
- 
             editoriales.add(edi);
         }
- 
     } catch (Exception e) {
         System.err.println("Error al listar editoriales: " + e.getMessage());
     }
- 
     return editoriales;
 }
 @Override
 public boolean insertar(Editorial editorial) {
     return false;
 }
- 
 @Override
 public Editorial buscar(String nit) {
     return null;
 }
- 
 @Override
 public boolean actualizar(Editorial editorial) {
     return false;
 }
- 
 @Override
 public boolean eliminar(String nit) {
     return false;
